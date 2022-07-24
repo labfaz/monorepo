3926a6e6078bcd7c2691594e2a1c310b8bf91e37
Nandosts <fernando.jsts@gmail.com>
2022-07-24 11:41:16 -0300

    :lipstick: Fix positioning on form

--- apps/client/src/Components/SignUp/Web/Step1/style.ts
+++ apps/client/src/Components/SignUp/Web/Step1/style.ts
@@ -161,10 +161,9 @@ export const SelectContainer = styled.label`
 `
 export const LeftSelectContainer = styled.label`
   width: 12.475rem;
-  margin-left: 2.2rem;
 
   position: absolute;
-
+  margin-top: 0.5rem;
   ${DesktopSmall(css`
     width: 8.33rem;
     bottom: -2.5rem;
