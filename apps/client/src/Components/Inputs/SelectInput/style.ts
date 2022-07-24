3221aae62b899f74962697d31db2f8661f3699ca
Nandosts <fernando.jsts@gmail.com>
2022-07-24 11:12:06 -0300

    :lipstick: Addapt select to change color when selected

--- apps/client/src/Components/Inputs/SelectInput/style.ts
+++ apps/client/src/Components/Inputs/SelectInput/style.ts
@@ -158,6 +158,10 @@ export const Container = styled.div<ContainerProps>`
       ${Mobile(css`
         font-size: var(--font-size-large);
       `)}
+
+      &[value=""] {
+        display: none;
+      }
     }
   }
 
