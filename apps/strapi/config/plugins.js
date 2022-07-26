c841c5d3df31cca435f3f892eac8912cdd039a3f
Kevin Souto <soutokevin@gmail.com>
2022-07-26 09:47:01 -0300

    Desabilita uso de AWS pelo strapi em dev

--- apps/strapi/config/plugins.js
+++ apps/strapi/config/plugins.js
@@ -1,13 +1,28 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS_REGION'),
-      params: {
-        Bucket: env('AWS_BUCKET'),
+module.exports = ({ env }) => {
+  if (env.bool('DISABLE_AWS')) {
+    return {
+      upload: {
+        config: {
+          providerOptions: {
+            localServer: { maxage: 300000 },
+          },
+        },
+      },
+    };
+  } 
+
+  return {
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
       },
     },
-  },
-});
+  };
+};
+ 
