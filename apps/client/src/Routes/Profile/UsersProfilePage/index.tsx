5885f46a014c69c116c0bea1441e886b3b427038
Nandosts <fernando.jsts@gmail.com>
2022-09-06 12:20:07 -0300

    :lock: Remove medicalReport from public profile

--- apps/client/src/Routes/Profile/UsersProfilePage/index.tsx
+++ apps/client/src/Routes/Profile/UsersProfilePage/index.tsx
@@ -14,15 +14,18 @@ export const ProfilePage: FC<UserProfilePageProps> = ({ id }) => {
 
   if (user.isLoading || user.isLoading) return <LoadingFullPage />;
 
-  if (!user.data)
+  if (!user.data) {
     return (
       <Error
         errorStatus={user.error.response?.status}
         errorMessage={user.error.response?.statusText}
-      />
+      /> 
     );
-
-  return <Display data={user.data} />;
+  } else {
+    // Evitando colocar esse dado no perfil aleio
+    user.data.artist.medicalReport = undefined;
+    return <Display data={user.data} />;
+  }
 };
 
 export default ProfilePage;
