65e5b82fd54fb9dfc7484614cf6c5dec9eb7cb63
Kevin Souto <soutokevin@gmail.com>
2022-10-14 22:37:39 -0300

    Corrigindo espacamento

--- apps/client/src/Components/SignUp/Mobile/Step18.tsx
+++ apps/client/src/Components/SignUp/Mobile/Step18.tsx
@@ -58,15 +58,14 @@ export const Step18: FC = () => {
 
           <InputCheckBoxContainer>
             <InputCheckbox type="checkbox" name="use_terms" value="sim">
-              Li e concordo com os
+              Li e concordo com os{' '}
               <a
                 href="/politica-de-privacidade.pdf"
                 target="_blank"
                 rel="noopener"
               >
-                {' '}
-                Termos de Uso{' '}
-              </a>
+                Termos de Uso
+              </a>{' '}
               e estou ciente e autorizo que os meus dado sejam usados única e
               exclusivamente para o projeto LabFaz.
             </InputCheckbox>
