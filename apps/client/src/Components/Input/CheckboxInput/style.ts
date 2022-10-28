707c92e19349dded31c7a7e290e9c272d53fc2e6
Kevin Souto <soutokevin@gmail.com>
2022-10-28 09:58:33 -0300

    Código duplicado

--- apps/client/src/Components/Input/CheckboxInput/style.ts
+++ apps/client/src/Components/Input/CheckboxInput/style.ts
@@ -2,15 +2,19 @@ import { Field } from 'formik';
 import styled, { css } from 'styled-components';
 import { DesktopSmall, Mobile } from 'Utils/breakpoints';
 
-export const Container = styled.div`
+export const Container = styled.label`
   display: inline-flex;
   cursor: pointer;
+
   position: relative;
+
   align-items: center;
   justify-content: space-between;
+
   ${Mobile(css`
     align-items: flex-start;
   `)}
+
   p {
     position: absolute;
     font-size: var(--font-size-small);
@@ -19,40 +23,50 @@ export const Container = styled.div`
     left: 2.2rem;
     color: red;
   }
+
   input {
     opacity: 0;
     position: absolute;
 
     z-index: 2;
+
     &:hover {
       cursor: pointer;
     }
   }
+
   .checkbox_input {
     width: 1rem;
     height: 1rem;
     padding: 5px;
+
     border: 1px solid rgba(250, 250, 250, 0.7);
+
     ${Mobile(css`
-      width: 30px;
+      width: 20px;
       height: 20px;
       padding: 7px;
     `)}
+
     background: rgba(0, 0, 0, 0.46);
+
     &:hover {
       cursor: pointer;
     }
   }
+
   .checkbox_input::after {
     content: '';
     width: 6px;
     height: 6px;
+
     border-radius: 50%;
     display: block;
     background-color: #fc0061;
     transform: scale(0);
     transition: transform 0.15s;
   }
+
   input:checked + .checkbox_input::after {
     transform: scale(1);
   }
@@ -61,22 +75,29 @@ export const Container = styled.div`
     font-weight: 600;
     color: var(--color-text-black);
     display: inline-block;
+
     color: #fafafa;
     margin-left: 1.2rem;
+
     line-height: 22px;
+
     padding: 0;
     margin-top: 0;
     margin-bottom: 0;
+
     ${DesktopSmall(css`
       font-size: var(--font-size-small);
     `)}
+
     ${Mobile(css`
       font-size: var(--font-size-large);
     `)}
+
     &:hover {
       cursor: pointer;
     }
   }
+
   a {
     font-weight: 600;
     color: var(--color-text-black);
